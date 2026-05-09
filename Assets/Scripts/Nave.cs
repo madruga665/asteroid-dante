695f1f7028bae8d1ca81f8d90b5e4e8028f21d07
madruga665 <madruga665@hotmail.com>
2026-05-09 12:09:02 -0300

    feat: add nave horizontal move

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
+    }
+
+    public void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
+    }
+}
